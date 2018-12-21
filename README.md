5b79520395b92b2ff05ff29f594518d94b9f011f
liqiang <liqiang@jd.com>
2018-12-21 11:40:34 +0800

    工程初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+quartz-distributed-redis-tool
+=================
+用于quartz集群部署使，同一时间保证仅单个节点执行job，基于Redis实现
---
+++ src/main/java/com/tipslee/distributed/Main.java
@@ -0,0 +1,10 @@
+package com.tipslee.distributed;
+
+/**
+ * @author liqiang
+ * @description
+ * @time 2018年12月21日
+ * @modifytime
+ */
+public class Main {
+}
